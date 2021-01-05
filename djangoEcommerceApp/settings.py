6464674ec77d1c66b547376b5d1596b40a216cae
amar0898 <70146865+amar0898@users.noreply.github.com>
2021-01-05 13:01:12 +0530

    Update settings.py

--- djangoEcommerceApp/settings.py
+++ djangoEcommerceApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'p4sa^vbma$$vet=l2*i&np(iy@$-87+vzp0cihh+=9u5c=yogd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['apna-bazzar.herokuapp.com']
 
 
 # Application definition
@@ -76,12 +76,13 @@ WSGI_APPLICATION = 'djangoEcommerceApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#  'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#      'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
